9deb8511dc57e3d2c29c7f611f370b117b5c39e0
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-11-05 21:34:05 +0100

    Create mm converter form

---
+++ src/components/ConverterForm.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+export default function ConverterForm() {
+  const [mmUnit, setMmUnit] = useState(0)
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const fd = new FormData(event.currentTarget)
+    const mm = fd.get('mm')
+
+    if (mm && Number(mm) > 0) {
+      setMmUnit(Number(mm))
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor="mm">
+          <input type="number" id="mm" name="mm" />
+        </label>
+      </p>
+    </form>
+  )
+}
