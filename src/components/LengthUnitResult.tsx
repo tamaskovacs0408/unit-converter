3cfa1b3502c2da397989746c6d6390db7d12b5c3
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-11-07 21:04:04 +0100

    Rendering units

--- src/components/LengthUnitResult.tsx
+++ src/components/LengthUnitResult.tsx
@@ -1,3 +1,14 @@
-export default function LengthUnitResult() {
-  
+import { LengthResultType } from '../types';
+
+export default function LengthUnitResult(props: LengthResultType) {
+    const { unit, lengthUnit, targetUnit } = props;
+
+    return (
+      <>
+      <h2>Converted result:</h2>
+        <p>{unit}</p>
+        <p>{lengthUnit}</p>
+        <p>{targetUnit}</p>
+      </>
+    )
 }
