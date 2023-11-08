488492b8a32172f5f81be7d0c563ee405dbaeddf
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-11-08 21:36:39 +0100

    Update converting function

--- src/components/LengthUnitResult.tsx
+++ src/components/LengthUnitResult.tsx
@@ -3,23 +3,24 @@ import { LengthResultType, UnitFactorsType } from '../types';
 export default function LengthUnitResult(props: LengthResultType) {
     const { unit, lengthUnit, targetUnit } = props;
 
-    const getConversionFactor = (unit: string, toMeter = false) => {
-      const unitFactors: UnitFactorsType = {
-        mm: 0.001,
-        cm: 0.01,
-        dm: 0.1,
-        m: 1,
-        km: 1000,
-        inch: 0.0254,
-        feet: 0.3048,
-        yard: 0.9144,
-        mile: 1609.34,
-      };
-    
-      return toMeter ? 1 / unitFactors[unit] : unitFactors[unit];
+    const unitFactors: UnitFactorsType = {
+      mm: 0.001,
+      cm: 0.01,
+      dm: 0.1,
+      m: 1,
+      km: 1000,
+      inch: 0.0254,
+      feet: 0.3048,
+      yard: 0.9144,
+      mile: 1609.34,
     };
+    
+    function convertLength(value: number, fromUnit: string, toUnit: string) {
+      return (value * unitFactors[fromUnit]) / unitFactors[toUnit];
+    }
+    
 
-    const result = getConversionFactor(targetUnit, true) * lengthUnit;
+    const result = Number(convertLength(lengthUnit, unit, targetUnit)?.toFixed(4));
 
     return (
       <>
