06ab2736043ddfaf01b8823924325b4f5215b42f
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-11-09 20:58:03 +0100

    Change toFixed, delete commented blocks

--- src/components/LengthUnitResult.tsx
+++ src/components/LengthUnitResult.tsx
@@ -20,7 +20,7 @@ export default function LengthUnitResult(props: LengthResultType) {
     }
     
 
-    const result = Number(convertLength(lengthUnit, unit, targetUnit)?.toFixed(4));
+    const result = Number(convertLength(lengthUnit, unit, targetUnit)?.toFixed(2));
 
     return (
       <>
@@ -29,48 +29,3 @@ export default function LengthUnitResult(props: LengthResultType) {
       </>
     )
 }
-
-/**
-  const getConversionFactor = (unit, toMeter = false) => {
-  const unitFactors = {
-    mm: 0.001,
-    cm: 0.01,
-    dm: 0.1,
-    m: 1,
-    km: 1000,
-    inch: 0.0254,
-    feet: 0.3048,
-    yard: 0.9144,
-    mile: 1609.34,
-  };
-
-  return toMeter ? 1 / unitFactors[unit] : unitFactors[unit];
-};
-
-const convertLength = () => {
-  const inputValueInMeters = parseFloat(inputValue) / 1000; // Converting to meters
-  let result;
-
-  switch (selectedUnit) {
-    case 'mm':
-      result = inputValueInMeters * 1000;
-      break;
-    case 'cm':
-      result = inputValueInMeters * 100;
-      break;
-    case 'dm':
-      result = inputValueInMeters * 10;
-      break;
-    case 'm':
-      result = inputValueInMeters;
-      break;
-    case 'km':
-      result = inputValueInMeters / 1000;
-      break;
-    default:
-      result = inputValue;
-  }
-
-  return result;
-};
- */
