71ee8ea1466c2dcd6d45c6cde8aae3ab74ea372a
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-11-28 21:14:58 +0100

    Update with destructured props

--- src/UI/Input.tsx
+++ src/UI/Input.tsx
@@ -1,19 +1,12 @@
 import { InputComponentType } from '../types';
 
 export default function Input(props: InputComponentType) {
-    const { unitRef, unitId } = props;
-    return (
-        <div>
-            <label htmlFor={unitId}>
-            <input
-                type='number'
-                id={unitId}
-                ref={unitRef}
-                step={0.01}
-                min={0}
-                defaultValue={0}
-            />
-            </label>
-        </div>
-    );
+    const { unitId, ...others } = props;
+  return (
+    <div>
+      <label htmlFor={unitId}>
+        <input id={unitId} {...others} />
+      </label>
+    </div>
+  );
 }
