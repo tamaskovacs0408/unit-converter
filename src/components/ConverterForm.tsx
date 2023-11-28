521f8ca26441e0eed8bab7d258d8f911ce22701d
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-11-28 21:15:05 +0100

    Add props

--- src/components/ConverterForm.tsx
+++ src/components/ConverterForm.tsx
@@ -5,46 +5,61 @@ import Input from '../UI/Input';
 import UnitSelector from './UnitSelector';
 
 export default function ConverterForm() {
-    const [lengthUnit, setLengthUnit] = useState(0);
-    const [unit, setUnit] = useState<string>('');
-    const [targetUnit, setTargetUnit] = useState('');
+  const [lengthUnit, setLengthUnit] = useState(0);
+  const [unit, setUnit] = useState<string>('');
+  const [targetUnit, setTargetUnit] = useState('');
 
-    const inputLengthUnit = useRef<HTMLInputElement>(null);
+  const inputLengthUnit = useRef<HTMLInputElement>(null);
 
-    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-        event.preventDefault();
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
-        const fd = new FormData(event.currentTarget);
-        const fromUnits = fd.getAll('from-units');
-        const toUnits = fd.getAll('to-units');
-        const data: {
-            [key: string]: FormDataEntryValue | FormDataEntryValue[];
-        } = Object.fromEntries(fd.entries());
-        data.fromUnitData = fromUnits;
-        data.toUnitData = toUnits;
-        const selectedUnit = data.fromUnitData[0].toString();
-        const selectedTargetUnit = data.toUnitData[0].toString();
+    const fd = new FormData(event.currentTarget);
+    const fromUnits = fd.getAll('from-units');
+    const toUnits = fd.getAll('to-units');
+    const data: {
+      [key: string]: FormDataEntryValue | FormDataEntryValue[];
+    } = Object.fromEntries(fd.entries());
+    data.fromUnitData = fromUnits;
+    data.toUnitData = toUnits;
+    const selectedUnit = data.fromUnitData[0].toString();
+    const selectedTargetUnit = data.toUnitData[0].toString();
 
-        const enteredInputLengthUnit = Number(inputLengthUnit.current!.value);
+    const enteredInputLengthUnit = Number(inputLengthUnit.current!.value);
 
-        setUnit(selectedUnit);
-        setTargetUnit(selectedTargetUnit);
-        setLengthUnit(enteredInputLengthUnit);
-    }
+    setUnit(selectedUnit);
+    setTargetUnit(selectedTargetUnit);
+    setLengthUnit(enteredInputLengthUnit);
+  }
 
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <UnitSelector convertTitle='From' unitId="from-units" unitsArray={lengthUnits}/>
-                <Input unitId="length-unit" unitRef={inputLengthUnit} />
-                <UnitSelector convertTitle='To' unitId="to-units" unitsArray={lengthUnits}/>
-                <button>Convert</button>
-            </form>
-            <LengthUnitResult
-                unit={unit}
-                targetUnit={targetUnit}
-                lengthUnit={lengthUnit}
-            />
-        </>
-    );
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <UnitSelector
+          convertTitle='From'
+          unitId='from-units'
+          unitsArray={lengthUnits}
+        />
+        <Input
+          unitId='length-unit'
+          type='number'
+          step={0.01}
+          min={0}
+          defaultValue={0}
+          ref={inputLengthUnit}
+        />
+        <UnitSelector
+          convertTitle='To'
+          unitId='to-units'
+          unitsArray={lengthUnits}
+        />
+        <button>Convert</button>
+      </form>
+      <LengthUnitResult
+        unit={unit}
+        targetUnit={targetUnit}
+        lengthUnit={lengthUnit}
+      />
+    </>
+  );
 }
