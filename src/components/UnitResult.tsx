7ca4e51511fab6a92902cd3b5e448785ba1016de
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-12-18 20:56:09 +0100

    delete commented block

--- src/components/UnitResult.tsx
+++ src/components/UnitResult.tsx
@@ -6,10 +6,6 @@ import unitConverter from '../lib/converter';
 export default function LengthUnitResult(props: LengthResultType) {
   const { unit, lengthUnit, targetUnit } = props;
 
-  // function convertLength(value: number, fromUnit: string, toUnit: string) {
-  //   return (value * unitFactors[fromUnit]) / unitFactors[toUnit];
-  // }
-
   const result = Number(
     unitConverter(lengthUnit, unit, targetUnit, lengthUnitFactors)?.toFixed(2)
   );
