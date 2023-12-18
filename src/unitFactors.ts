b8bf4fe5a582bc6e656db19cd22a037b02a702c7
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-12-18 20:44:28 +0100

    Create new file for unit factors, add length factors

---
+++ src/unitFactors.ts
@@ -0,0 +1,13 @@
+import { UnitFactorsType } from "./types"
+
+export const lengtUnitFactors: UnitFactorsType = {
+  mm: 0.001,
+  cm: 0.01,
+  dm: 0.1,
+  m: 1,
+  km: 1000,
+  in: 0.0254,
+  ft: 0.3048,
+  yd: 0.9144,
+  mi: 1609.34,
+}
