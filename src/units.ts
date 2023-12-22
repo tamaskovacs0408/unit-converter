7ec2b5b53176da3f14519ea0b94be24374ea3d1b
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-12-22 15:26:04 +0100

    Add units for weight and volume

--- src/units.ts
+++ src/units.ts
@@ -1,1 +1,3 @@
 export const lengthUnits = ['mm', 'cm', 'dm', 'm', 'km', 'in', 'ft', 'yd', 'mi'];
+export const weightUnits = ['g', 'dkg', 'kg', 'ton', 'oz', 'lb'];
+export const volumeUnits = ['ml', 'cl', 'dl', 'l', 'cbm', 'fl oz', 'pt', 'qt', 'gal'];
