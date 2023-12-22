b4c8635e55363d9bde2a13af9d1980a421ea73af
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-12-22 15:25:45 +0100

    Add unit factors for weight and volume

--- src/unitFactors.ts
+++ src/unitFactors.ts
@@ -11,3 +11,24 @@ export const lengthUnitFactors: UnitFactorsType = {
   yd: 0.9144,
   mi: 1609.34,
 }
+
+export const weightUnitFactors: UnitFactorsType = {
+  g: 0.001,             // gram
+  dkg: 0.01,            // decagram
+  kg: 1,                // kilogram as the base unit for weight
+  ton: 1000,            // metric ton
+  oz: 0.0283495,        // ounce
+  lb: 0.453592,         // pound
+};
+
+export const volumeUnitFactors: UnitFactorsType = {
+  ml: 0.001,            // milliliter
+  cl: 0.01,             // centiliter
+  dl: 0.1,              // deciliter
+  l: 1,                 // liter as the base unit for volume
+  cbm: 1000,            // cubic meter
+  'fl oz': 0.0295735,   // fluid ounce
+  pt: 0.473176,         // pint
+  qt: 0.946353,         // quart
+  gal: 3.78541,         // gallon
+};
