16e5613678d8048b985c84371ec82b69833862e3
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-12-22 15:27:15 +0100

    import and set context

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -1,11 +1,14 @@
+import { useContext } from 'react';
 import { ButtonProps } from '../types';
+import { SelectedUnitContext } from '../store/selectedUnitContext';
 
 export default function Button(props: ButtonProps) {
-  const { id, imageUrl, title } = props;
+  const { id, title } = props;
+
+  const { handleUnitSelect } = useContext(SelectedUnitContext);
 
   return (
-    <button id={id}>
-      {/* <img src={imageUrl} alt={id} /> */}
+    <button id={id} onClick={() => handleUnitSelect(id)}>
       {title}
     </button>
   );
