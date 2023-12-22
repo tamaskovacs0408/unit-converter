04d7519e2ad092120e4626393b7b74ba19cd3e84
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-12-22 15:26:49 +0100

    Add wrapper and import component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.scss';
 import ConverterForm from './components/ConverterForm';
 import Selector from './components/Selector';
+import { SelectedUnitProvider } from './store/selectedUnitContext';
 
 function App() {
   return (
-    <>
-      <Selector />
-      <ConverterForm />
-    </>
+    <SelectedUnitProvider>
+      <>
+        <Selector />
+        <ConverterForm />
+      </>
+    </SelectedUnitProvider>
   );
 }
 
