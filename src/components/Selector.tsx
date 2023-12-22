eacac4e45782441f5c3a6c3b81a4cf679ed9da34
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-12-22 15:27:03 +0100

    Create component

---
+++ src/components/Selector.tsx
@@ -0,0 +1,11 @@
+import Button from "../UI/Button";
+
+export default function Selector() {
+  return (
+    <header>
+      <Button id="length" title="Length" />
+      <Button id="weight" title="Weight" />
+      <Button id="volume" title="Volume" />
+    </header>
+  )
+}
