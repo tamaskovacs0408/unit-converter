274e8cec2ce9e92f3ec5b06564deeb8bbb2c84f2
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-12-23 10:02:07 +0100

    Add condition to selected unit factor

--- src/components/UnitResult.tsx
+++ src/components/UnitResult.tsx
@@ -1,13 +1,30 @@
+import { useContext } from 'react';
 import { ResultType } from '../types';
-
 import { lengthUnitFactors, weightUnitFactors, volumeUnitFactors } from '../unitFactors';
 import unitConverter from '../lib/converter';
+import {SelectedUnitContext} from '../store/selectedUnitContext';
 
 export default function UnitResult(props: ResultType) {
   const { unit, fromUnit, targetUnit } = props;
 
+  const { selectedUnitState } = useContext(SelectedUnitContext);
+
+  let unitFactor: Record<string, number> = {};
+
+  if (selectedUnitState === 'length') {
+    unitFactor = lengthUnitFactors;
+  }
+
+  if (selectedUnitState === 'weight') {
+    unitFactor = weightUnitFactors;
+  }
+
+  if (selectedUnitState === 'volume') {
+    unitFactor = volumeUnitFactors;
+  }
+
   const result = Number(
-    unitConverter(fromUnit, unit, targetUnit, lengthUnitFactors)?.toFixed(2)
+    unitConverter(fromUnit, unit, targetUnit, unitFactor)?.toFixed(2)
   );
 
   return (
