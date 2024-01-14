06658e90a418412f74966db5e5546015522e064e
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-01-14 10:31:46 +0100

    import styles

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,8 +1,9 @@
 import Button from "../UI/Button";
+import classes from "./Selector.module.scss";
 
 export default function Selector() {
   return (
-    <header>
+    <header className={classes.header}>
       <Button id="length" title="Length" />
       <Button id="weight" title="Weight" />
       <Button id="volume" title="Volume" />
