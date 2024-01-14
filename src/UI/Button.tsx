55cba5cfb19c14b735d9f3b437d7ce4d721bde1b
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-01-14 15:47:16 +0100

    import style

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -2,13 +2,15 @@ import { useContext } from 'react';
 import { ButtonProps } from '../types';
 import { SelectedUnitContext } from '../store/selectedUnitContext';
 
+import classes from './Button.module.scss';
+
 export default function Button(props: ButtonProps) {
   const { id, title } = props;
 
   const { handleUnitSelect } = useContext(SelectedUnitContext);
 
   return (
-    <button id={id} onClick={() => handleUnitSelect(id)}>
+    <button className={classes.button} id={id} onClick={() => handleUnitSelect(id)}>
       {title}
     </button>
   );
