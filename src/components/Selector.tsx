b234209f40fbd18765646a740160faf75c755526
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-03-20 15:38:08 +0100

    Structure update

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -3,10 +3,12 @@ import classes from "./Selector.module.scss";
 
 export default function Selector() {
   return (
-    <header className={classes.header}>
-      <Button id="length" title="Length" />
-      <Button id="weight" title="Weight" />
-      <Button id="volume" title="Volume" />
-    </header>
-  )
+    <div className={classes["selector-wrapper"]}>
+      <header>
+        <Button id='length' title='Length' />
+        <Button id='weight' title='Weight' />
+        <Button id='volume' title='Volume' />
+      </header>
+    </div>
+  );
 }
