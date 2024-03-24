793d9639b37d4bacedfd20c79c9969d655027cc5
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-03-24 14:49:21 +0100

    Create new units objects

--- src/units.ts
+++ src/units.ts
@@ -1,3 +1,131 @@
 export const lengthUnits = ['mm', 'cm', 'dm', 'm', 'km', 'in', 'ft', 'yd', 'mi'];
 export const weightUnits = ['g', 'dkg', 'kg', 'ton', 'oz', 'lb'];
 export const volumeUnits = ['ml', 'cl', 'dl', 'l', 'cbm', 'fl oz', 'pt', 'qt', 'gal'];
+export const convertableLengthUnits = [
+    {
+        id: 1,
+        unit: 'mm',
+        title: 'Millimeter',
+    },
+    {
+        id: 2,
+        unit: 'cm',
+        title: 'Centimeter',
+    },
+    {
+        id: 3,
+        unit: 'dm',
+        title: 'Decimeter',
+    },
+    {
+        id: 4,
+        unit: 'm',
+        title: 'Meter',
+    },
+    {
+        id: 5,
+        unit: 'km',
+        title: 'Kilometer',
+    },
+    {
+        id: 6,
+        unit: 'in',
+        title: 'Inch',
+    },
+    {
+        id: 7,
+        unit: 'ft',
+        title: 'Foot',
+    },
+    {
+        id: 8,
+        unit: 'yd',
+        title: 'Yard',
+    },
+    {
+        id: 9,
+        unit: 'mi',
+        title: 'Mile',
+    }
+];
+
+export const convertableWeightUnits = [
+    {
+        id: 1,
+        unit: 'g',
+        title: 'Gramms',
+    },
+    {
+        id: 2,
+        unit: 'dkg',
+        title: 'Decagramms',
+    },
+    {
+        id: 3,
+        unit: 'kg',
+        title: 'Kilograms',
+    },
+    {
+        id: 4,
+        unit: 'ton',
+        title: 'Tons',
+    },
+    {
+        id: 5,
+        unit: 'oz',
+        title: 'Ounces',
+    },
+    {
+        id: 6,
+        unit: 'lb',
+        title: 'Pounds',
+    }
+];
+
+export const convertableVolumeUnits = [
+    {
+        id: 1,
+        unit: 'ml',
+        title: 'Milliliters',
+    },
+    {
+        id: 2,
+        unit: 'cl',
+        title: 'Centiliters',
+    },
+    {
+        id: 3,
+        unit: 'dl',
+        title: 'Deciliters',
+    },
+    {
+        id: 4,
+        unit: 'l',
+        title: 'Liters',
+    },
+    {
+        id: 5,
+        unit: 'cbm',
+        title: 'Cubic Meters',
+    },
+    {
+        id: 6,
+        unit: 'fl oz',
+        title: 'Fluid Ounces',
+    },
+    {
+        id: 7,
+        unit: 'pt',
+        title: 'Pints',
+    },
+    {
+        id: 8,
+        unit: 'qt',
+        title: 'Quarts',
+    },
+    {
+        id: 9,
+        unit: 'gal',
+        title: 'Gallons',
+    }
+];
