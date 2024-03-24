db1a4891607dfe1e4259f1dff3318fa68ffeff76
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-03-24 20:35:26 +0100

    Render units based on selected unit

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,15 +1,42 @@
+import { useContext } from "react";
 import { ModalPorps } from "../types";
 import classes from "./Modal.module.scss";
+import {SelectedUnitContext} from '../store/selectedUnitContext';
+import {convertableLengthUnits, convertableWeightUnits, convertableVolumeUnits, lengthUnits, weightUnits, volumeUnits} from '../units';
 
 export default function Modal(props: ModalPorps) {
   const { showModal, toggleModal } = props;
+
+  const { selectedUnitState } = useContext(SelectedUnitContext);
+
+  let convertableUnit;
+
+  switch (selectedUnitState) {
+    case 'length':
+      convertableUnit = convertableLengthUnits;
+      break;
+    case 'weight':
+      convertableUnit = convertableWeightUnits;
+      break;
+    case 'volume':
+      convertableUnit = convertableVolumeUnits;
+      break;
+  }
+
+
+
+
 
   return (
     showModal && (
       <div className={classes["modal-wrapper"]} onClick={toggleModal}>
         <div className={classes["modal-container"]}>
             <button onClick={toggleModal}>X</button>
-          <h1>Modal</h1>
+            <ul>
+              {convertableUnit.map(data => (
+                <li key={data.id}>{data.unit} - {data.title}</li>
+              ))}
+            </ul>
         </div>
       </div>
     )
