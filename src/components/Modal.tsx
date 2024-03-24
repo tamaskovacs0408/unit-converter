ada52265022eb02429bcfe0dad3d8a7efbf0dcd4
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-03-24 15:44:56 +0100

    Add base elements and functionality

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,15 +1,17 @@
 import { ModalPorps } from "../types";
-import classes from "./Modal.module.scss"
+import classes from "./Modal.module.scss";
 
 export default function Modal(props: ModalPorps) {
-    const { showModal, toggleModal } = props;
+  const { showModal, toggleModal } = props;
 
-    return (
-        showModal &&
-        <div className={classes["modal-wrapper"]}>
-            <div className={classes["modal-container"]}>
-                <h1>Modal</h1>
-            </div>
+  return (
+    showModal && (
+      <div className={classes["modal-wrapper"]} onClick={toggleModal}>
+        <div className={classes["modal-container"]}>
+            <button onClick={toggleModal}>X</button>
+          <h1>Modal</h1>
         </div>
-    );
+      </div>
+    )
+  );
 }
