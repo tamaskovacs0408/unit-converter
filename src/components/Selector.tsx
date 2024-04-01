455f38ed084680c21b10894eeb03d791408448a0
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-04-01 13:18:12 +0200

    Refactoring component

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,13 +1,28 @@
+import { useContext } from "react";
 import Button from "../UI/Button";
 import classes from "./Selector.module.scss";
+import { SelectedUnitContext } from "../store/selectedUnitContext";
 
 export default function Selector() {
+  const buttons = [
+    { id: "length", title: "Length" },
+    { id: "weight", title: "Weight" },
+    { id: "volume", title: "Volume" },
+  ];
+
+  const { handleUnitSelect } = useContext(SelectedUnitContext);
+
   return (
     <div className={classes["selector-wrapper"]}>
       <header>
-        <Button id='length' title='Length' />
-        <Button id='weight' title='Weight' />
-        <Button id='volume' title='Volume' />
+      {buttons.map(button => (
+            <Button
+              key={button.id}
+              id={button.id}
+              title={button.title}
+              handleUnitSelect={handleUnitSelect}
+            />
+          ))}
       </header>
     </div>
   );
