7e2ca02efe7a8128e89031518a12f9cd0bcee70c
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-05-21 19:46:35 +0200

    Remove title

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,9 @@ import { SelectedUnitProvider } from './store/selectedUnitContext';
 function App() {
   return (
     <SelectedUnitProvider>
-      <>
-      <h1>Unit Converter</h1>
         <Selector />
         <ModalPortal />
         <ConverterForm />
-      </>
     </SelectedUnitProvider>
   );
 }
