bc03460ba53d09b0aa6464711c26ce593ac63c0b
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-05-21 19:34:25 +0200

    Add 10 power function for bigints

--- src/lib/converter.ts
+++ src/lib/converter.ts
@@ -1,8 +1,14 @@
-export default function unitConverter(
+export function unitConverter(
   value: number,
   from: string,
   to: string,
   unitFactor: Record<string, number>
 ): number {
   return (value * unitFactor[from]) / unitFactor[to];
+}
+ // minus: 0.0000001, plus: 1000000000000000000000
+export function toPowerOf10(number: number) {
+  const power = Math.floor(Math.log10(Math.abs(number)));
+  const adjustedNumber = number / Math.pow(10, power);
+  return Number(adjustedNumber.toFixed(0)) * Math.pow(10, power);
 }
