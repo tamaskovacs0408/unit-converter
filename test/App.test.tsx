0f594e33a8ea739cc1fc4768739a1feb16ea0f79
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-07-28 11:46:26 +0200

    Delete files

--- test/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../src/App";
-
-it("should have title", () => {
-    render(<App />);
-    const mainTitle = screen.queryByText(/unit converter/i);
-    expect(mainTitle).toBeDefined();
-})
