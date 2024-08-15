a75df901682d3afc4b7303c659fc90d65c4b5aa6
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-08-15 20:45:40 +0200

    Update router with createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,43 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate, NavLink } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet, NavLink } from 'react-router-dom';
 import "@/App.scss";
 import ConverterForm from "@/components/ConverterForm";
 import ModalPortal from "@/components/ModalPortal";
 import Selector from "@/components/Selector";
 import { SelectedUnitProvider } from "@/store/selectedUnitContext"
 
-const App: React.FC = React.memo(() => (
-  <Router>
-    <SelectedUnitProvider>
-      <main>
-        <Routes>
-          <Route path="/" element={<Selector />} />
-          <Route path="/converter/:unitType" element={
-            <>
-              <NavLink to="/">Back to selectors</NavLink>
-              <ConverterForm />
-              <ModalPortal />
-            </>
-          } />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </main>
-    </SelectedUnitProvider>
-  </Router>
-));
+const Layout = () => (
+  <SelectedUnitProvider>
+    <main>
+      <Outlet />
+    </main>
+  </SelectedUnitProvider>
+);
 
-// const App = React.memo(() => (
-//   <SelectedUnitProvider>
-//     <main>
-//       <Selector />
-//       <ModalPortal />
-//       <ConverterForm />
-//     </main>
-//   </SelectedUnitProvider>
-// ));
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Selector />,
+      },
+      {
+        path: 'converter/:unitType',
+        element: (
+          <>
+            <ConverterForm />
+            <ModalPortal />
+          </>
+        ),
+      },
+    ],
+  },
+]);
+
+const App: React.FC = () => (
+  <RouterProvider router={router} />
+);
 
 export default App;
